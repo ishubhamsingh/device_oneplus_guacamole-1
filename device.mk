0094dfb39823327302b3f405deb13d2d65ad3942
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-10 12:26:11 +0000

    guacamole: build vibrator service from source
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- device.mk
+++ device.mk
@@ -36,3 +36,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl:64 \
+    android.hardware.vibrator@1.0-service
